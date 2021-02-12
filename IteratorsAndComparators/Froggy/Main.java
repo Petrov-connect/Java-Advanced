6499f0b702d8edc5e7d67da80a227ee64aab4890
Petrov-connect <Petrov.connect@gmail.com>
2021-02-12 23:15:42 +0200

    java code

---
+++ IteratorsAndComparators/Froggy/Main.java
@@ -0,0 +1,18 @@
+package IteratorsAndComparators.Froggy;
+//created by J.M.
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Lake lake = new Lake(Arrays.stream(scan.nextLine().trim().split(", ")).mapToInt(Integer::parseInt).toArray());
+        Iterator<Integer> iterator= lake.iterator();
+        while (iterator.hasNext()) {
+            String current = iterator.next().toString();
+            System.out.print(iterator.hasNext() ? String.format("%s, ",current) : current);
+        }
+    }
+}
