714bc4183a92d6bc97a484b861b356bb04ebac4c
Petrov-connect <Petrov.connect@gmail.com>
2021-02-12 18:25:52 +0200

    java code

---
+++ IteratorsAndComparators/ListyIterator/ListyIterator.java
@@ -0,0 +1,38 @@
+package IteratorsAndComparators.ListyIterator;
+//created by J.M.
+
+import java.util.List;
+
+public class ListyIterator {
+
+    private int index;
+    private final List<String> data;
+
+    public ListyIterator(List<String> data) {
+        this.data = data;
+    }
+
+
+    public  void print(){
+        validatePrint();
+        System.out.println(this.data.get(index));
+    }
+
+    private void validatePrint(){
+        if(this.data.isEmpty()){
+            throw new IllegalStateException("Invalid Operation!");
+        }
+    }
+
+    public boolean move(){
+        if(hasNext()){
+            index++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasNext(){
+        return index<data.size()-1;
+    }
+}
