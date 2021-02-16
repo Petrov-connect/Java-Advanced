30ab498ca9b3378678616f72eec199c894ab8d4b
Petrov-connect <Petrov.connect@gmail.com>
2021-02-16 14:19:57 +0200

    java code

---
+++ BasicAlgorithms/BinarySearch/Main.java
@@ -0,0 +1,12 @@
+package BasicAlgorithms.BinarySearch;
+//created by J.M.
+
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
