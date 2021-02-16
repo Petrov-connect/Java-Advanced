0c30b7a00f1f5eedcf970c3d02c9c50f347ea04d
Petrov-connect <Petrov.connect@gmail.com>
2021-02-16 15:25:02 +0200

    java code

--- BasicAlgorithms/MergeSort/Main.java
+++ BasicAlgorithms/MergeSort/Main.java
@@ -14,38 +14,40 @@ public class Main {
 
     private static int[] mergeSort(int[] array) {
 
-        if(array.length==1){
+        if (array.length == 1) {
             return array;
         }
-        int halfIndex = array.length/2;
-        int[]firstPartition = Arrays.copyOfRange(array,0,halfIndex);
-        int[]secondPartition = Arrays.copyOfRange(array,halfIndex,array.length);
+        int halfIndex = array.length / 2;
+        int[] firstPartition = Arrays.copyOfRange(array, 0, halfIndex);
+        int[] secondPartition = Arrays.copyOfRange(array, halfIndex, array.length);
 
-        firstPartition=mergeSort(firstPartition);
-        secondPartition=mergeSort(secondPartition);
-        return  mergeTwoSortedArrays(firstPartition,secondPartition);
+        firstPartition = mergeSort(firstPartition);
+        secondPartition = mergeSort(secondPartition);
+
+        return mergeTwoSortedArrays(firstPartition, secondPartition);
     }
 
     private static int[] mergeTwoSortedArrays(int[] firstPartition, int[] secondPartition) {
-        int []merged = new int[firstPartition.length+secondPartition.length];
-        int firstIndex = 0;
-        int secondIndex = 0;
-        while (firstIndex<firstPartition.length&&secondIndex<secondPartition.length){
-            if(firstPartition[firstIndex]<secondPartition[secondIndex]){
-                merged[firstIndex+secondIndex]=firstPartition[firstIndex];
+
+        int[] merged = new int[firstPartition.length + secondPartition.length];
+        int firstIndex = 0 , secondIndex = 0;
+
+        while (firstIndex < firstPartition.length && secondIndex < secondPartition.length) {
+            if (firstPartition[firstIndex] < secondPartition[secondIndex]) {
+                merged[firstIndex + secondIndex] = firstPartition[firstIndex];
                 firstIndex++;
-            }else{
-                merged[firstIndex+secondIndex]=secondPartition[secondIndex];
+            } else {
+                merged[firstIndex + secondIndex] = secondPartition[secondIndex];
                 secondIndex++;
             }
         }
-        while (firstIndex<firstPartition.length){
-            merged[firstIndex+secondIndex]=firstPartition[firstIndex];
-                firstIndex++;
+        while (firstIndex < firstPartition.length) {
+            merged[firstIndex + secondIndex] = firstPartition[firstIndex];
+            firstIndex++;
         }
-        while (secondIndex<secondPartition.length){
-            merged[firstIndex+secondIndex]=secondPartition[secondIndex];
-                secondIndex++;
+        while (secondIndex < secondPartition.length) {
+            merged[firstIndex + secondIndex] = secondPartition[secondIndex];
+            secondIndex++;
         }
         return merged;
     }
