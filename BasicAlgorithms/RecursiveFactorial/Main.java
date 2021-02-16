61f8545826b688696b629f2014bfe0b411ab3618
Petrov-connect <Petrov.connect@gmail.com>
2021-02-16 14:18:20 +0200

    java code

---
+++ BasicAlgorithms/RecursiveFactorial/Main.java
@@ -0,0 +1,18 @@
+package BasicAlgorithms.RecursiveFactorial;
+//created by J.M.
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        System.out.println(factorial(n));
+    }
+
+    private static long factorial(int n) {
+        return n == 0 ? 1 : n * factorial(n - 1);
+    }
+}
