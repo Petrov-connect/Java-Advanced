4869c11b0228422c7795a686b7900d2ad766a246
Petrov-connect <Petrov.connect@gmail.com>
2021-02-16 16:11:11 +0200

    java code

--- BasicAlgorithms/Quicksort/Main.java
+++ BasicAlgorithms/Quicksort/Main.java
@@ -8,34 +8,29 @@ class Main {
 
         Scanner scan = new Scanner(System.in);
         int[] array = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-        sort(array, 0, array.length-1);
+        sort(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array).replaceAll("[\\[\\],]", ""));
     }
-    //routine to sort the array partitions recursively
+
     static void sort(int[] subArray, int startIndex, int endIndex) {
         if (startIndex < endIndex) {
-            //partition the array around pi=>partitioning index and return pi
-            int pi = partition(subArray, startIndex, endIndex);
-            // sort each partition recursively
-            sort(subArray, startIndex, pi - 1);
-            sort(subArray, pi + 1, endIndex);
+            int pivot = partition(subArray, startIndex, endIndex);
+            sort(subArray, startIndex, pivot - 1);
+            sort(subArray, pivot + 1, endIndex);
         }
     }
-    //selects last element as pivot, pi using which array is partitioned.
+
     static int partition(int[] subArray, int startIndex, int endIndex) {
-        int pi = subArray[endIndex];
-        int i = (startIndex - 1); // smaller element index
+        int pivot = subArray[endIndex];
+        int i = (startIndex - 1);
         for (int j = startIndex; j < endIndex; j++) {
-            // check if current element is less than or equal to pi
-            if (subArray[j] <= pi) {
+            if (subArray[j] <= pivot) {
                 i++;
-                // swap intArray[i] and intArray[j]
                 int temp = subArray[i];
                 subArray[i] = subArray[j];
                 subArray[j] = temp;
             }
         }
-        // swap intArray[i+1] and intArray[high] (or pi)
         int temp = subArray[i + 1];
         subArray[i + 1] = subArray[endIndex];
         subArray[endIndex] = temp;
