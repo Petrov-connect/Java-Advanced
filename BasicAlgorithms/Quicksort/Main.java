b51c690e7d262b4afbf4112834d3999f624cd7f7
Petrov-connect <Petrov.connect@gmail.com>
2021-02-19 17:12:38 +0200

    java code

--- BasicAlgorithms/Quicksort/Main.java
+++ BasicAlgorithms/Quicksort/Main.java
@@ -9,11 +9,13 @@ class Main {
 
         Scanner scan = new Scanner(System.in);
         int[] array = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
         sort(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array).replaceAll("[\\[\\],]", ""));
     }
 
     static void sort(int[] subArray, int startIndex, int endIndex) {
+
         if (startIndex < endIndex) {
             int pivot = partition(subArray, startIndex, endIndex);
             sort(subArray, startIndex, pivot - 1);
@@ -22,8 +24,10 @@ class Main {
     }
 
     static int partition(int[] subArray, int startIndex, int endIndex) {
+
         int pivot = subArray[endIndex];
         int i = (startIndex - 1);
+
         for (int j = startIndex; j < endIndex; j++) {
             if (subArray[j] <= pivot) {
                 i++;
