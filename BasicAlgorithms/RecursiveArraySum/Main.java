550a144efcfa9603eb162213466b788b1867dd0c
Petrov-connect <Petrov.connect@gmail.com>
2021-02-19 17:13:08 +0200

    java code

--- BasicAlgorithms/RecursiveArraySum/Main.java
+++ BasicAlgorithms/RecursiveArraySum/Main.java
@@ -9,10 +9,12 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
         int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
         System.out.println(sum(numbers, 0));
     }
 
     private static int sum(int[] numbers, int index) {
+
         return index == numbers.length - 1 ? numbers[index] : numbers[index] + sum(numbers, index + 1);
     }
 }
