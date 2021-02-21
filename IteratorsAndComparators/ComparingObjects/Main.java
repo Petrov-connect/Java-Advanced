61e77ed591d64a8207ed24d9c60028062acab110
Petrov-connect <Petrov.connect@gmail.com>
2021-02-21 15:12:32 +0200

    java code

--- IteratorsAndComparators/ComparingObjects/Main.java
+++ IteratorsAndComparators/ComparingObjects/Main.java
@@ -22,6 +22,7 @@ public class Main {
 
         int n = Integer.parseInt(scan.nextLine());
         Person findPerson = personInfo.get(n - 1);
+
         long equals = personInfo.stream().filter(e -> findPerson.compareTo(e) == 0).count();
 
         if (equals == 1) {
